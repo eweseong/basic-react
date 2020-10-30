8d61c0f7add602331e47f18fc5c5a5d00502e972
eweseongyeoh <eweseongyeoh@airasia.com>
2020-10-30 20:28:51 +0800

    Updates

---
+++ precache-manifest.0d2160d89a459ee51ca4f406560e76a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0093a0d3ad4935458c89e1d5434734bb",
+    "url": "/basic-react/index.html"
+  },
+  {
+    "revision": "cb01d9073168d01afd2f",
+    "url": "/basic-react/static/css/main.4f348fdc.chunk.css"
+  },
+  {
+    "revision": "2f1e69f7dc0db5085373",
+    "url": "/basic-react/static/js/2.c897444c.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/basic-react/static/js/2.c897444c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb01d9073168d01afd2f",
+    "url": "/basic-react/static/js/main.a3a3509c.chunk.js"
+  },
+  {
+    "revision": "c00650521d1f16ac0d20",
+    "url": "/basic-react/static/js/runtime-main.66fa7f78.js"
+  },
+  {
+    "revision": "067dbb15abc7dba3e4cf4d1da99bc2f8",
+    "url": "/basic-react/static/media/netflix-logo.067dbb15.svg"
+  }
+]);
